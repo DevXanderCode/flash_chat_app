e4ddf09dc18c105415e19f9b67e8b59185304169
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-02-02 17:33:09 +0100

    started working ont the login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext build) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
+            ),
+            SizedBox(height: 48.0),
+            TextField(
+              onChanged: (value) {
+                //  Do something with the user input
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your email',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 20.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //  Do Something with the user input.
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
