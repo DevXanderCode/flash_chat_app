62d08405a1931e1a82d029d40a9220808d30b511
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-02-02 16:24:35 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          textTheme: const TextTheme(
+        bodyText1: TextStyle(color: Colors.black54),
+      )),
+    );
+  }
+}
