a2e02f062dddc7ad9b377118c027c6bc371d5d75
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-02-03 10:32:22 +0100

    created a basic structure of the login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -71,6 +71,47 @@ class _LoginScreenState extends State<LoginScreen> {
                     Radius.circular(32.0),
                   ),
                 ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.lightBlueAccent,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30.0),
+                ),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //  Implement Login functionality
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
               ),
             )
           ],
