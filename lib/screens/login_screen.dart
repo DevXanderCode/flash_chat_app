056f431667ad2c57ffdcd130f4c40cc903faf69c
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-02-16 17:05:03 +0100

    used the Text field decoration constant in the login and registration screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -28,10 +28,12 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             SizedBox(height: 48.0),
             TextField(
-                onChanged: (value) {
-                  //  Do something with the user input
-                },
-                decoration: kTextFieldDecoration.copyWith(hintText: '')),
+              onChanged: (value) {
+                //  Do something with the user input
+              },
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
+            ),
             SizedBox(
               height: 8.0,
             ),
@@ -39,37 +41,8 @@ class _LoginScreenState extends State<LoginScreen> {
               onChanged: (value) {
                 //  Do Something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                hintStyle: TextStyle(color: Colors.blueGrey.shade500),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.lightBlueAccent,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.lightBlueAccent,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
-                ),
-              ),
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password'),
             ),
             SizedBox(
               height: 24.0,
