48148d2bafcff0ab8b9be53df11b981e08a4be4d
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-02-22 10:09:42 +0100

    added some logic to the main file to initialze the firebase app and also worked on the registration logic as well

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flash_chat_app/screens/registration_screen.dart';
 import 'package:flash_chat_app/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(const FlashChat());
@@ -13,7 +14,37 @@ class FlashChat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          //      check errors
+          // if (snapshot.hasError) {
+          //   return null;
+          // }
+          //  once completed show my application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return FlashChatApp();
+          }
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('Loading'),
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class FlashChatApp extends StatelessWidget {
+  const FlashChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,7 +12,24 @@ class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
 
-  void getCurrentUser() {}
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
